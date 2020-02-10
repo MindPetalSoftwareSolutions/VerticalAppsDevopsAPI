3c70d9bc3bca1378e208481240c8a9e2ab2c00c2
Xavier Coleman <coleman_xavier@bah.com>
2020-02-10 08:34:02 -0500

    first commit

---
+++ Controllers/ProcessController.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace VerticalAppsDevopsAPI.Controllers{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ProcessController : ControllerBase{
+        
+    }
+}
---
+++ Models/Authentication.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace VerticalAppsDevopsAPI.Models{
+    public class Authentication{
+        
+        [Required]
+        public string grant_type { get; set; }
+        [Required]
+        public string client_id { get; set; }
+        [Required]
+        public string refresh_token { get; set; }
+
+    }
+}
